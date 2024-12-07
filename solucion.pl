bb8ab4fd2412097ecdb9e12f5bde14b5b69dd1a4
GonzaloPurita <gpurita@frba.utn.edu.ar>
2024-12-07 10:13:23 -0300

    Terminado hasta el punto 3 (complemento)

--- solucion.pl
+++ solucion.pl
@@ -1,6 +1,9 @@
 % PUNTO 1
-vive(juan, casa(120)).  %metros cuadrados
-vive(nico, depto(3, 2)).  % (ambientes, banios)
+%vive(Persona, casa(Metros)).
+%vive(Persona, depto(Ambientes, Banios)).
+%vive(Persona, loft(AnioConstruccion)).
+vive(juan, casa(120)).
+vive(nico, depto(3, 2)).
 vive(alf, depto(3, 1)).
 vive(julian, loft(2000)).
 vive(vale, depto(4, 1)).
@@ -14,6 +17,7 @@ barrio(vale, flores).
 barrio(fer, flores).
 
 % PUNTO 2
+% todas las personas de un barrio, viven en propiedades copadas
 barrioCopado(Barrio):-
     barrio(_, Barrio),
     forall(barrio(Persona, Barrio), (vive(Persona, Propiedad), propiedadCopada(Propiedad))).
@@ -24,6 +28,7 @@ propiedadCopada(depto(_, Banio)):- Banio > 1.
 propiedadCopada(loft(Anio)):- Anio > 2015.
 
 % PUNTO 3
+% no existe una propiedad en este barrio que sea barata
 barrioCaro(Barrio):-
     barrio(_, Barrio),
     not(((barrio(Persona, Barrio), vive(Persona, Propiedad)), propiedadBarata(Propiedad))).
